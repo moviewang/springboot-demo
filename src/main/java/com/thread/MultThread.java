4fcbc10b3e13f79a06069b1a117db49f18238cf8
moviewang <2314165649@qq.com>
2018-01-26 12:08:30 +0800

    executor

--- src/main/java/com/thread/MultThread.java
+++ src/main/java/com/thread/MultThread.java
@@ -2,6 +2,8 @@ package com.thread;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Movie on 2018/1/25.
@@ -10,8 +12,11 @@ public class MultThread {
 
     public static void main(String[] args) {
 //        single();
-        fixThread();
+//        fixThread();
+//        cacheThread();
+        scheduleThread();
     }
+
 
 
 
@@ -57,6 +62,41 @@ public class MultThread {
                 }
             });
         }
+    }
 
+    /**
+     * 相当于new thread 每个任务一个线程,区别在与上一个任务在60l秒内完成，则会服用此线程，
+     * 否则，新建一个线程
+     */
+    public static void cacheThread() {
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            int index = i;
+
+            try {
+                Thread.sleep(70000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            cachedThreadPool.execute(() -> {
+                System.out.println(index  + "\tthread name:" + Thread.currentThread().getName());
+            });
+        }
+    }
+
+    /**
+     * 定时周期性threadpool
+     */
+    public static void scheduleThread() {
+        ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
+        scheduledThreadPool.schedule(() -> {
+            System.out.println("delay 3s");
+        }, 3, TimeUnit.SECONDS);
+
+        ScheduledExecutorService scheduleFixrate = Executors.newScheduledThreadPool(2);
+        scheduleFixrate.scheduleAtFixedRate(() -> {
+            System.out.println("3s");
+        }, 1, 3, TimeUnit.SECONDS);
     }
 }
